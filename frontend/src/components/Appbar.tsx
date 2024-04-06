18ce953da305749770360beb03e8812ab95b38cf
Mishragini <mishragini11.com>
2024-04-06 09:50:53 +0530

    replaces anchor tags with Link

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useUser } from "../hooks";
-import { useNavigate , useLocation} from "react-router-dom";
+import { useNavigate , useLocation, Link} from "react-router-dom";
 
 export default function Appbar() {
   const location = useLocation();
@@ -24,9 +24,10 @@ export default function Appbar() {
   return (
     <>
      {!shouldHideAppbar && <div className="flex justify-between p-6 border-b ">
-      <a href="/blogs" className="text-3xl font-bold">
-        Write<span className="text-blue-500">Hub</span>
-      </a>
+        
+        <Link to={"/blogs"} className="text-3xl font-bold">
+         Write<span className="text-blue-500">Hub</span>
+        </Link>
       <div className="flex">
         <button onClick={()=>{navigate('/myProfile')}}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="gray" className="w-10 h-10 mr-2">
          <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
--- frontend/src/components/BlogCard.tsx
+++ frontend/src/components/BlogCard.tsx
@@ -1,10 +1,16 @@
+import { Link } from "react-router-dom";
+
 export function BlogCard({title,content,createdOn,id}:{title:string,content:string,createdOn:string,id:string}){
 return(
     <div className="my-4">
         <div className="text-3xl font-semibold my-2">{title}</div>
         <div className="my-2 text-slate-500">Posted on {createdOn}</div>
 
-        <div>{content} <a  className="text-blue-500 hover:text-blue-600"href={`/blog/${id}`}>Read More..</a></div>
+        <div>{content} 
+        <Link to={`/blog/${id}`} className="text-blue-500 hover:text-blue-600 ml-2">
+        Read More..
+        </Link>
+        </div>
 
     </div>
 )
