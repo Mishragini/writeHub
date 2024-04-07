62d38baade95832adea6b2b491053afa50485d9f
Mishragini <mishragini11.com>
2024-04-07 13:00:13 +0530

    modifies FullBlog to show content in paragraphs

--- frontend/src/components/FullBlog.tsx
+++ frontend/src/components/FullBlog.tsx
@@ -1,41 +1,65 @@
 import { useNavigate } from "react-router-dom";
 import { AuthorCard } from "./AuthorCard";
 import { useUser } from "../hooks";
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 
 export function FullBlog({ blog }: any) {
-  const {user}=useUser();
-  const[isOwner,setIsOwner]=useState(false);
-  useEffect(()=>{
-    user.posts.map((post)=>{
-      if(post.id===blog.id){
+  const { user } = useUser();
+  const [isOwner, setIsOwner] = useState(false);
+
+  useEffect(() => {
+    
+    user.posts.map((post) => {
+      if (post.id === blog.id) {
         setIsOwner(true);
       }
-    })
-  },[user])
+    });
+  }, [user]);
+
   const createdOnDate = new Date(blog.createdOn);
   const formattedCreatedOn = createdOnDate.toDateString();
-  const navigate=useNavigate();
+  const paragraphs = blog.content.split(/\n/);
+  const contentWithLineBreaks = paragraphs.map((paragraph:string) => (
+    <Fragment >
+    {paragraph}
+    <br />
+  </Fragment>
+  ));
+
+  const navigate = useNavigate();
+
   return (
     <div className="m-8 grid grid-cols-1 lg:grid-cols-12">
       <div className="col-span-9">
         <div className="flex items-center">
-        <div className="text-5xl font-extrabold">{blog.title}</div>
-        <button onClick={()=>{
-          const authorId=blog.authorId;
-          navigate(`/author/${authorId}`)
-        }}className="block lg:hidden bg-green-500 hover:bg-green-600 rounded-lg p-2 text-white ml-4"> About the Author</button>
+          <div className="text-5xl font-extrabold">{blog.title}</div>
+          <button
+            onClick={() => {
+              const authorId = blog.authorId;
+              navigate(`/author/${authorId}`);
+            }}
+            className="block lg:hidden bg-green-500 hover:bg-green-600 rounded-lg p-2 text-white ml-4"
+          >
+            {" "}
+            About the Author
+          </button>
         </div>
-        <div className="mt-4  text-gray-400">Posted on . {formattedCreatedOn}</div>
-        <div className="mt-4  text-gray-700">{blog.content}</div>
-        {(isOwner)&& <button className="text-white bg-blue-500 hover:bg-blue-600 px-4 py-2 rounded-lg mt-2"onClick={()=>{navigate(`/edit/${blog.id}`)}}>Edit</button>}
+        <div className="mt-4 text-gray-400">Posted on . {formattedCreatedOn}</div>
+        <div className="mt-4 text-gray-700">{contentWithLineBreaks}</div>
+        {isOwner && (
+          <button
+            className="text-white bg-blue-500 hover:bg-blue-600 px-4 py-2 rounded-lg mt-2"
+            onClick={() => {
+              navigate(`/edit/${blog.id}`);
+            }}
+          >
+            Edit
+          </button>
+        )}
       </div>
       <div className="col span-3 hidden lg:block">
         <AuthorCard name={blog.author.name} about={blog.author.about} />
       </div>
-      
     </div>
   );
 }
-
-
--- frontend/src/pages/AddBlog.tsx
+++ frontend/src/pages/AddBlog.tsx
@@ -31,7 +31,7 @@ export function AddBlog() {
             </div>
             <label  className="block mb-2 text-md font-medium text-gray-900">Your Blog</label>
             <textarea id="message" rows={8} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 "placeholder="Write your thoughts here..." onChange={(e)=>{setDescription(e.target.value)}}></textarea>
-            <button onClick={handleBlogPost}type="button" className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mt-4">Publish</button>
+            <button onClick={handleBlogPost} type="button" className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mt-4">Publish</button>
         </div>
     );
 }
